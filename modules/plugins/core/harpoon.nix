74ef01f961b94e65f183172ce6284150ce1fec84
Nikita Elfimov <t2mann@yandex.ru>
2026-01-12 12:35:19 +0500

    harpoon

--- modules/plugins/core/harpoon.nix
+++ modules/plugins/core/harpoon.nix
@@ -1,140 +1,161 @@
+{ inputs, ... }:
 {
   flake.modules.nixvim.harpoon = {
     plugins = {
       harpoon = {
         enable = true;
-        lazyLoad.settings.keys = [
-          {
-            __unkeyed-1 = "<leader>sm";
-            __unkeyed-2.__raw = ''
-              function()
-                require('telescope').extensions.harpoon.marks()
-              end
-            '';
-            desc = "[S]earch [H]arpoon marks with Telescope";
-          }
-          {
-            __unkeyed-1 = "<leader>Ma";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.mark").add_file()
-                require('notify')('Harpoon mark added')
-              end
-            '';
-            desc = "Harpoon [M]ark [A]dd";
-          }
-          {
-            __unkeyed-1 = "<leader>Md";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.mark").rm_file()
-                require('notify')('Harpoon mark deleted')
-              end
-            '';
-            desc = "Harpoon [M]ark [D]elete";
-          }
-          {
-            __unkeyed-1 = "<leader>Mc";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.mark").clear_all()
-                require('notify')('Harpoon marks cleared')
-              end
-            '';
-            desc = "Harpoon [M]arks [C]lear";
-          }
-          {
-            __unkeyed-1 = "<leader>m";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").toggle_quick_menu()
-              end
-            '';
-            desc = "Harpoon toggle quick [m]enu";
-          }
-          {
-            __unkeyed-1 = "<leader>1";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(1)
-              end
-            '';
-            desc = "Harpoon navigate to [1]st file";
-          }
-          {
-            __unkeyed-1 = "<leader>2";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(2)
-              end
-            '';
-            desc = "Harpoon navigate to [2]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>3";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(3)
-              end
-            '';
-            desc = "Harpoon navigate to [3]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>4";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(4)
-              end
-            '';
-            desc = "Harpoon navigate to [4]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>5";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(5)
-              end
-            '';
-            desc = "Harpoon navigate to [5]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>6";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(6)
-              end
-            '';
-            desc = "Harpoon navigate to [6]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>7";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(7)
-              end
-            '';
-            desc = "Harpoon navigate to [7]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>8";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(8)
-              end
-            '';
-            desc = "Harpoon navigate to [8]nd file";
-          }
-          {
-            __unkeyed-1 = "<leader>9";
-            __unkeyed-2.__raw = ''
-              function()
-                require("harpoon.ui").nav_file(9)
-              end
-            '';
-            desc = "Harpoon navigate to [9]nd file";
-          }
-        ];
       };
     };
+
+    keymaps = [
+      {
+        key = "<leader>sm";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require('telescope').extensions.harpoon.marks()
+          end
+        '';
+        options = {
+          desc = "[S]earch [H]arpoon marks with Telescope";
+        };
+        mode = [
+          "n"
+        ];
+      }
+
+      {
+        key = "<leader>Ma";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():add()
+            require('notify')('Harpoon mark added')
+          end
+        '';
+        options = {
+          desc = "Harpoon [M]ark [A]dd";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>m";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon'.ui:toggle_quick_menu(require'harpoon':list())
+          end
+        '';
+        options = {
+          desc = "Harpoon toggle quick [m]enu";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>1";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(1)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [1]st file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>2";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(2)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [2]nd file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>3";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(3)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [3]rd file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>4";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(4)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [4]th file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>5";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(5)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [5]th file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>6";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(6)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [6]th file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>7";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(7)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [7]th file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>8";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(8)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [8]th file";
+        };
+        mode = [ "n" ];
+      }
+      {
+        key = "<leader>9";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            require'harpoon':list():select(9)
+          end
+        '';
+        options = {
+          desc = "Harpoon navigate to [9]th file";
+        };
+        mode = [ "n" ];
+      }
+    ];
   };
 }
